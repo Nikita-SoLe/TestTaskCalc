436297822dca685ad686227d9e9c129a2b3b9755
Nikita <sole.yfan@gmail.com>
2023-11-16 19:19:23 +0300

    0.1.1

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        while (scan.hasNextLine()) {
-            String str = scan.nextLine();
-            System.out.println(calc(str));
-        }
+        String str = scan.nextLine();
+        System.out.println(calc(str));
 
     }
 
